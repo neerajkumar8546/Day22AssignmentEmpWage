f23e0c8d7db16272ffbe4d2e3359e67b977f4e69
unknown <neerajkumar8546@gmail.com>
2020-06-16 19:12:37 +0530

    Cal empWage using Switch Case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,6 +2,28 @@ public class EmpWageBuilder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("* Welcome To Employee Wage Computation Program *");
+		final int is_FULL_TIME=1;
+		final int is_Part_Time=2;
+		final int emp_RATE_PER_HOUR=20;
+		
+		int empHrs=0;
+		int empWage=0;
+
+		int check=(int)Math.floor(Math.random()*10)%3;
+		
+		switch (check)
+		{
+			case is_FULL_TIME:
+				empHrs=4;
+				break;
+			case is_Part_Time:
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+		}
+	
+		empWage=empHrs*emp_RATE_PER_HOUR;
+		System.out.println("Employee Wage = "+empWage);
 	}
 }
