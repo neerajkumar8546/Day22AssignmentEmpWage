1c00193cdd1db4fa973145c4a2e382e5bcd0f098
unknown <neerajkumar8546@gmail.com>
2020-06-16 19:01:48 +0530

    Display Emp Present or absent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,6 +2,14 @@ public class EmpWageBuilder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("* Welcome To Employee Wage Computation Program *");
+		double checkAtt=Math.floor(Math.random()*10)%2;
+		if(checkAtt==1)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 	}
 }
