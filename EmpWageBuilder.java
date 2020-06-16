0c0a97cfa64133daa9a70346b8d56f2bcb6d3a85
unknown <neerajkumar8546@gmail.com>
2020-06-16 19:08:39 +0530

    Add Part Time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,6 +2,27 @@ public class EmpWageBuilder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("* Welcome To Employee Wage Computation Program *");
+		int is_FULL_TIME=1;
+		int is_Part_Time=2;
+		int emp_RATE_PER_HOUR=20;
+		
+		int empHrs=0;
+		int empWage=0;
+
+		double check=Math.floor(Math.random()*10)%3;
+		if(check==is_FULL_TIME)
+		{
+			empHrs=4;
+		}
+		else if(check==is_Part_Time)
+		{
+			empHrs=8;
+		}
+		else
+		{
+			empHrs=0;
+		}
+		empWage=empHrs*emp_RATE_PER_HOUR;
+		System.out.println("Employee Wage = "+empWage);
 	}
 }
