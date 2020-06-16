96a5e4374e23ccd708a8eaebe5d97da39b7e6644
unknown <neerajkumar8546@gmail.com>
2020-06-16 19:15:37 +0530

    Cal Wage for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,6 +2,36 @@ public class EmpWageBuilder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("* Welcome To Employee Wage Computation Program *");
+		final int is_FULL_TIME=1;
+		final int is_Part_Time=2;
+		final int emp_RATE_PER_HOUR=20;
+		final int num_Of_Working_Days=20;
+		int empHrs=0;
+		int empWage=0;
+		int totalEmpWage=0;
+
+		
+		for (int i=0;i<num_Of_Working_Days;i++)
+		{
+			int check=(int)Math.floor(Math.random()*10)%3;
+
+			switch (check)
+      	{
+         	case is_FULL_TIME:
+            	empHrs=4;
+            	break;
+         	case is_Part_Time:
+            	empHrs=8;
+         	   break;
+      	   default:
+   	         empHrs=0;
+	      }
+			empWage=empHrs*emp_RATE_PER_HOUR;
+			totalEmpWage=totalEmpWage+empWage;
+			System.out.println("Employee Wage = "+empWage);
+
+		}
+
+		System.out.println("Total Employee Wage = "+totalEmpWage);
 	}
 }
